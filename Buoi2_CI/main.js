60f0e4788da3b53d4e848ea21c5df184025af90b
Dante <nguyenchinhan1298@gmail.com>
2022-02-18 20:35:11 +0700

    test

---
+++ Buoi2_CI/main.js
@@ -0,0 +1,48 @@
+
+const btnSubmit = document.getElementById("btn-primary");
+const inputText = document.getElementById("input-text");
+const myForm = document.getElementById("my-form")
+// function clickMe () {
+//    console.log ("Nhân nè");
+//    clickPrimary();
+// }
+// function clickPrimary () {
+//     console.log ("clickPrimary");
+// }
+// btnSubmit.addEventListener("click", () => {
+//     console.log("Arrow click");
+// });
+// btnSubmit.addEventListener("click", function () {
+//     console.log("Nomal click");
+// });
+// inputText.addEventListener("blur", (event) => {
+//     console.log("blur-", event);
+// });
+// inputText.addEventListener("change", (event) => {
+//     console.log("change-", event);
+// }); 
+// myForm.addEventListener("submit", (e) => {
+//     console.log(e); 
+// }) 
+
+// function myFuncion(param) {
+//     param('Học lập trình');
+// }
+
+// function myCallback(value) {
+// console.log('Value:', value)
+// }
+// myFuncion (myCallback);
+// var courses = [
+//     'Javascript',
+//     'PHP',
+//     'Ruby'
+// ];
+//  courses.map(function () {
+
+//  })
+class Car {
+    engine;
+    speed;
+    
+}
